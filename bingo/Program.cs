56f8294978b4712cfa7ff4ca6f968a453560885d
veronezzi159 <lucasveronezzi3@gmail.com>
2024-04-28 13:59:46 -0300

    identifica ganhadores

--- bingo/Program.cs
+++ bingo/Program.cs
@@ -2,7 +2,8 @@
 
 int[] numeros = new int[99],sorteados = new int[99];
 int[,] cartela1 = new int[5, 5], aux = new int[5,5];
-bool ganhador_coluna = false, ganhador_linha = false;
+bool ganhador_coluna = false, ganhador_linha = false, ganhador_cartela = false;
+int rodada_coluna = 0, rodada_linha = 0, rodada_cartela = 0;
 
 int[,] criarCartela()
 {
@@ -133,48 +134,130 @@ void imprimirNumerosSorteados(int num,int indice)
     Console.ReadKey();
 }
 
-void verificaCartela(int[,] matriz, int[,] matriz_aux, ref int contador_linha, ref int contador_coluna, ref int contador_cartela)
+int verificaLinha(int qt_jogadores, int qt_cartelas,int[,] count_linhas) 
 {
-    ganhador_coluna = false;
-    ganhador_linha = false;
+    int contador = 0;
 
-    for (int rodadas = 0; rodadas < 99; rodadas++)
+    for (int i = 0; i < qt_jogadores ; i++)
     {
-        int sorteado = sorteados[rodadas];
-        imprimirNumerosSorteados(sorteado, rodadas);
-
-        for (int linhas = 0; linhas < 5; linhas++)
+        for (int j = 0; j < qt_cartelas; j++)
         {
-            for(int colunas = 0;colunas < 5; colunas++)
+            if (count_linhas[i,j] == 1) 
             {
-                if (matriz[linhas,colunas] == sorteado)
-                {
-                    matriz_aux[linhas, colunas] = 1;
-                    contador_cartela++;
-                }
-                if (ganhador_coluna == false && matriz_aux[0, colunas] == 1 && matriz_aux[1,colunas] == 1 && matriz_aux[2,colunas] == 1 && matriz_aux[3,colunas] == 1 && matriz_aux[4,colunas] == 1)
-                {
-                    ganhador_coluna = true;
-                    contador_coluna = rodadas;
-                    break;
-                }
+                contador++;
             }
-            if (ganhador_linha == false && matriz_aux[linhas,0] == 1 && matriz_aux[linhas, 1] == 1 && matriz_aux[linhas, 2] == 1 && matriz_aux[linhas, 3] == 1 && matriz_aux[linhas, 4] == 1)
-            {
-                ganhador_linha = true;                
-                contador_linha = rodadas;
-
-                break;
-            }
-            imprimirCartela(matriz,matriz_aux);
-        }
-        if (contador_cartela == 25)
-        {
-            break;
         }
     }
 
+    return contador;
+}
+int verificaColuna(int qt_jogadores, int qt_cartelas, int[,] count_colunas)
+{
+    int contador = 0;
 
+    for (int i = 0; i < qt_jogadores; i++)
+    {
+        for (int j = 0; j < qt_cartelas; j++)
+        {
+            if (count_colunas[i, j] == 1)
+            {
+                contador++;
+            }
+        }
+    }
+
+    return contador;
+}
+
+int verificaGanahdorCartela(int qt_jogadores, int qt_cartelas, int[,] count_cartela)
+{
+    int contador = 0;
+
+    for (int i = 0; i < qt_jogadores; i++)
+    {
+        for (int j = 0; j < qt_cartelas; j++)
+        {
+            if (count_cartela[i, j] == 1)
+            {
+                contador++;
+            }
+        }
+    }
+
+    return contador;
+}
+
+void verificaCartela(int[,] matriz, int[,] matriz_aux,int sorteado ,ref int contador_linha, ref int contador_coluna, ref int contador_cartela)
+{
+    if (ganhador_coluna == false) 
+    {
+        rodada_coluna = sorteado;
+    }
+    if(ganhador_linha = false)
+    {
+        rodada_linha = sorteado;
+    }
+    if (ganhador_cartela == false)
+    {
+        rodada_cartela = sorteado;
+    }
+    for (int linhas = 0; linhas < 5; linhas++)
+    {
+        for(int colunas = 0;colunas < 5; colunas++)
+        {
+            if (matriz[linhas,colunas] == sorteado)
+            {
+                    matriz_aux[linhas, colunas] = 1;
+                    contador_cartela++;
+                if (contador_cartela == 25 && ganhador_cartela == false)
+                {
+                    ganhador_cartela = true;
+                    contador_cartela = 1;
+                }
+                else
+                {
+                    if (ganhador_cartela == true && rodada_cartela == sorteado && contador_cartela == 25)
+                    {
+                        contador_cartela = 1;
+                    }
+                }
+            }
+            if (ganhador_coluna == false && matriz_aux[0, colunas] == 1 && matriz_aux[1,colunas] == 1 && matriz_aux[2,colunas] == 1 && matriz_aux[3,colunas] == 1 && matriz_aux[4,colunas] == 1)
+            {
+                ganhador_coluna = true;
+                contador_coluna = 1;
+                break;
+            }
+            else
+            {
+                if (ganhador_coluna == true && rodada_coluna == sorteado && matriz_aux[0, colunas] == 1 && matriz_aux[1, colunas] == 1 && matriz_aux[2, colunas] == 1 && matriz_aux[3, colunas] == 1 && matriz_aux[4, colunas] == 1)
+                {
+                    contador_coluna = 1;
+                    break;
+                }
+            }
+        }
+        if (ganhador_linha == false && matriz_aux[linhas,0] == 1 && matriz_aux[linhas, 1] == 1 && matriz_aux[linhas, 2] == 1 && matriz_aux[linhas, 3] == 1 && matriz_aux[linhas, 4] == 1)
+        {
+            ganhador_linha = true;  
+            if (contador_linha == 0)
+            {
+                contador_linha = 1;
+            }
+
+        }
+        else
+        {
+            if (ganhador_linha == true && rodada_linha == sorteado && matriz_aux[linhas, 0] == 1 && matriz_aux[linhas, 1] == 1 && matriz_aux[linhas, 2] == 1 && matriz_aux[linhas, 3] == 1 && matriz_aux[linhas, 4] == 1)
+            {
+                if (contador_linha == 0)
+                {
+                    contador_linha = 1;
+                }
+            }
+        }
+        imprimirCartela(matriz,matriz_aux);
+    }
 }
 
 int qtd_jogadores = quantidadeJogador();
@@ -204,20 +287,40 @@ for (int i = 0; i < qtd_jogadores; i++)
 }
 
 sorteiaNumeros();
+int count_ganahdores_linhas = 0, count_ganhadores_coluna = 0, count_ganhadores_cartela = 0;
+for (int rodadas = 0; rodadas < 99; rodadas++)
+{
+    int sorteado = sorteados[rodadas];
+    imprimirNumerosSorteados(sorteado, rodadas);
+    for (int i = 0; i < qtd_jogadores; i++) {//jogador
 
-for (int i = 0; i < qtd_jogadores; i++) {
+        for (int j = 0; j < qtd_cartelas; j++) // cartela
+        {
+            int[,] cartela = vetor_cartelas[i][j];        
+            int[,] cartela_aux = vetor_cartelas_aux[i][j];        
+            verificaCartela(cartela, cartela_aux,sorteado ,ref contador_linha[i,j], ref contador_coluna[i,j], ref contador_cartela[i,j]);
+            Console.WriteLine(" jogador " + i + ", cartela " + j + ", contador linha: " + contador_linha[i,j]);
 
-    for (int j = 0; j < qtd_cartelas; j++)
-    {
-        int[,] cartela = vetor_cartelas[i][j];        
-        int[,] cartela_aux = vetor_cartelas_aux[i][j];        
-        verificaCartela(cartela, cartela_aux, ref contador_linha[i,j], ref contador_coluna[i,j], ref contador_cartela[i,j]);
-        Console.WriteLine(" jogador " + i + ", cartela " + j + ", contador linha: " + contador_linha[i,j]);
+            // Console.ReadLine();
 
-        Console.ReadLine();
-
+        }
     }
+    if (ganhador_linha == true)
+    {
+       count_ganahdores_linhas  = verificaLinha(qtd_jogadores, qtd_cartelas, contador_linha);
+    }
+    if (ganhador_coluna == true)
+    {
+        count_ganhadores_coluna = verificaColuna(qtd_jogadores,qtd_cartelas, contador_coluna);
+    }
+    if (ganhador_cartela == true)
+    {
+        count_ganhadores_cartela = verificaGanahdorCartela(qtd_jogadores,qtd_cartelas, contador_cartela);
+        break;
+    }
+ 
 }
+
 
 //cartelaDoJogadorUm[0][0]
 
